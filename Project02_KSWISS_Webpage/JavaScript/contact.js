9483b4f63e864412023de07c70eec85197036efa
Lauren <peng2ma@gmail.com>
2022-09-04 23:36:15 +0800

    add contact page and modified the btns on the index page

---
+++ Project02_KSWISS_Webpage/JavaScript/contact.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    console.log($('.tab'));
+    console.log($('ul#tabs li a'));
+    $('.tab:first').show();
+    $('#tabs li a:first').addClass('active');
+    $('ul#tabs li a').click(function () {
+        var index = $('ul#tabs li a').index($(this));
+        console.log(index);
+        $('ul#tabs li a').removeClass('active');
+        $('.tab').hide();
+        $('.tab').eq(index).show();
+        $(this).addClass('active');
+    });
+});
