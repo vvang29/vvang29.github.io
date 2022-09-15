930c5dc4163be8786d2feeb64726f31b58318782
Lauren <peng2ma@gmail.com>
2022-09-15 15:16:04 +0800

    add media query RWD and fix append bugs

--- Project05_todolist.htm/todo.js
+++ Project05_todolist.htm/todo.js
@@ -3,39 +3,49 @@ $(document).ready(function(){
     $("#addItem").on("click",function(){
         
         //clone的預設是false，所以函式不會執行，需改為true
-        var newItem = $(".item").eq(0).clone(true); //複製最後一項
-        newItem.find("div").text($("#addItemVal").val());
+        // var newItem = $(".item").eq(0).clone(true); //複製最後一項
+        // newItem.find("div").text($("#addItemVal").val());
         //下面這個方法會改到文字和叉叉的內文，後改用find()去找某個特定的元素就解決問題了
         // newItem.children($(".itemContent")).text($("#addItemVal").val());
         //加上新項目到清單中
-        $(".listArea").children($(".item").eq(-1).after(newItem));
-
-
-
-        // var newwItem = 
-
-
+        // $(".listArea").children($(".item").eq(-1).after(newItem));
+        let add = $("#addItemVal").val();
+        if(add == ''){  //使用者需輸入內容才可新增項目
+            alert("Please fill in the box.");
+        }else{
+            var inputValue = $("#addItemVal").val();
+            $(".listArea").append(
+              `<div class="item">
+              <div class="itemContent">${inputValue}</div>
+              <button type="button" class="deleteItem">X</button>
+              </div> `
+            );
+        }
 
     });//addItem function end
 
+    
 
-
-    $(".deleteItem").on("click",function(){
+    // $(".deleteItem").on("click",function(){
+    //     $(this).parent($("div")).remove();
+    // })
+    // $(".item").on("click",".deleteItem",function(){
+    //     $(this).parent($("div")).remove();
+    // })
+//以上兩個方法會使新增的項目無法執行刪除功能
+//解決辦法：on內部需要加上selector(append中的節點是在整個文檔加載完之後開始添加，頁面不會爲append的元素初始化添加點擊事件，故使用這種方式動態添加的節點中的click事件沒有生效。)
+    $(document).on("click",".deleteItem",function(){
         $(this).parent($("div")).remove();
     })
-$
 
 // $(".checkbox").on("click",function(){
-//     console.log(123);
 //     if( $(".checkbox").prop("checked") == true){
-//         console.log(456);
 //         // $(".itemContent").toggleClass("crossLine");
 //         // 這個方法會影響到其他items
 //         //$(this).siblings("div").addClass("crossLine");
 //         $(this).next("div").addClass("crossLine");
 //         console.log($(this));
 //     }else{
-//         console.log(789);
 //         //這個方法會影響到其他items
 //         //$(this).siblings("div").removeClass("crossLine");
 //         $(this).next("div").removeClass("crossLine");
@@ -46,13 +56,11 @@ $
 
 
 //莫名解掉上面的bug...
-//把範圍改成全部便解決問題:)
-$(".item").click(function(){
-    // $(this).prev("input").attr("checked","checked");
-    // $(this).toggleClass("crossLine");
+//1把範圍改成全部便解決問題:)
+//2:append異步問題，已改善
+$(document).on("click",".item",function(){
     $(this).find($(".itemContent")).toggleClass("crossLine");
 })
-
 
 
 
